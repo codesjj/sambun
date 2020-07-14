1c32574b0d7618ec2f67cc6d32c64337cc826401
codesjj <codesjj@gmail.com>
2020-07-14 11:33:53 +0900

    d

--- beta/script2.js
+++ beta/script2.js
@@ -790,6 +790,36 @@ class Deck extends React.Component{
             _e.selected=false;
         })
     }
+        
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('componentWillReceiveProps');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate');
+        return true / false;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+    
 }
 
 ReactDOM.render(   
